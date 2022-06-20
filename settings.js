3f6ab36d1d384ad5aea91cfcc6a04ab7151609b0
Nikila122 <107881503+Nikila122@users.noreply.github.com>
2022-06-20 23:33:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94717030729']
+global.premium = ['94717030729']
+global.ownernomer = '94717030729'
 global.ownername = 'Dark Alpha'
 global.botname = 'Bixby MD'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
